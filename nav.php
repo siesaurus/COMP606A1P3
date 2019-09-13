2b0adf16b85b009fb2a68f0015d5ee498abe112d
Callum <callumgott@gmail.com>
2019-09-13 14:42:13 +1200

    Edited the nav page, it now has extra divs in the main nav bar on the top of the screen to position the content to the main of the screen, I added an asset folder which contains our logo for the business, the logo is embedded into nav page. Also commented out the login form inside the nav bar, as we have a link to the login page we probably don't need it? can always change back

--- nav.php
+++ nav.php
@@ -3,9 +3,37 @@
     Features navbar login which directs to login.php also
  -->
 <div class ="topnav" id="navbarNav">
-<a href="Home.php">Home</a>
-<a href="LoginPage.php">Login</a>
-<a href="Registration.php">Register</a>
+
+<div class ="mainbar" id="barMain">
+
+<!--
+    Created a logo from this website https://hatchful.shopify.com
+    and made it usable as a home page button.
+ -->
+<div class ="logo" id="logoHome">
+<a href="Home.php"><img src="Assets/logo.png" alt="Logo Home-Page link" height="100" width="125"></a>
+</div>
+
+<div class ="navbar" id="barNav">
+<!--
+    Unordered list for the links for nav bar
+ -->
+<ul>
+<li><a href="Home.php">Home</a></li>
+<li><a href="LoginPage.php">Login</a></li>
+<li><a href="Registration.php">Register</a></li>
+<li><a href="AboutUs.php">About Us</a></li>
+<li><a href="ContactUs.php">Contact Us</a></li>
+
+
+</ul>
+</div>
+</div>
+<!--
+    Commented out for now as we already have a login link? 
+    Can always put it back in later.
+ -->
+<!--
 <div class="login-container">
 <form action="Login.php" method = "POST">
 
@@ -14,6 +42,7 @@
 <button type = "submit">Login</button>
 </form>
 </div>
+-->
 </div>
 
 
