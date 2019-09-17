0dcc67d0ac3c2c8716310bcd14e202de13299fa9
Callum <callumgott@gmail.com>
2019-09-17 16:08:16 +1200

    just edited more css, not a lot extra

--- Registration.php
+++ Registration.php
@@ -10,30 +10,34 @@
 <head>
 <body>
 <title>Registration</title><div class ="container col-md-4">
-    <h2>Register Now</h2>
-    <p> Please enter your details below</p>
-<form action="Register.php" method="POST">
-    <table>
-        <tr>
-            <td>First name: </td>
-            <td><input type="text" name="FirstName"></td>
-        </tr>
-        <tr>
-            <td>Last name: </td>
-            <td><input type="text" name="LastName"></td>
-        </tr>
-        <tr>
-            <td>Email address:</td>
-            <td><input type="text" name="Email"></td>
-        </tr>
-        <tr>
-            <td>Password: </td>
-            <td> <input type="password" name="Passwd"></td>
-        </tr>
-        <tr>
-        <td><input type="submit" value = "Submit"></td>
-        </tr>
-        </table>
+
+    <div class="formContainer">
+        <h2>Register Now</h2>
+        <p> Please enter your details below</p>
+        <form action="Register.php" method="POST">
+            <table>
+                <tr>
+                    <td>First name: </td>
+                    <td><input type="text" name="FirstName"></td>
+                </tr>
+                <tr>
+                    <td>Last name: </td>
+                    <td><input type="text" name="LastName"></td>
+                </tr>
+                <tr>
+                    <td>Email address:</td>
+                    <td><input type="text" name="Email"></td>
+                </tr>
+                <tr>
+                    <td>Password: </td>
+                    <td> <input type="password" name="Passwd"></td>
+                </tr>
+                <tr>
+                <td><input type="submit" value = "Submit"></td>
+                </tr>
+                </table>
         </form>
+    </div>
+</div>
 </body>
 </html>
