d68e92975be4dcf61534626f82e9c8c854de2870
siesaurus <52937221+siesaurus@users.noreply.github.com>
2019-09-19 12:35:18 +1200

    made 1 login page for all, needs formatting

--- LoginPage.php
+++ LoginPage.php
@@ -7,21 +7,50 @@
 <html>
 <head>
 <body>
+<div id="tabs">
+<ul>
+    <li><a href="#tabs-1">Customer Login</a></li>
+    <li><a href="#tabs-2">Therapist Login</a></li>
+</ul>
+<div id ="tabs-1">
+    <div class ="container col-md-4">
+        <div class ="LoginFormContainer">
+            <h2>Enter your details to login</h2>
+
+            <form action="Login.php" method="POST">
+                <table>
+                <tr>
+                <td>Username: </td>
+                <td><input type ="text" placeholder="Username" name = "Username"> </td>
+                </tr>
+                <tr>
+                <td>Password: </td>
+                <td><input type = "password" placeholder="Password" name = "Password"></td>
+                </tr>   
+                <tr>
+                <td></td>
+                <td><input type = "submit" value ="Login"></td>
+                </tr>
+                </table>
+            </form>
+        </div>
+    </div>
+</div>
+<div id = "tabs-2">
 <title>Login</title>
 <div class ="container col-md-4">
-
-    <div class ="formContainer">
+    <div class ="TherapistFormContainer">
         <h2>Enter your details to login</h2>
 
-        <form action="Login.php" method="POST">
+        <form action="therapistlogin.php" method="POST">
         <table>
         <tr>
         <td>Username: </td>
-        <td><input type ="text" placeholder="Username" name = "Username"> </td>
+        <td><input type ="text" placeholder="Username" name = "TherUsername"> </td>
         </tr>
         <tr>
         <td>Password: </td>
-        <td><input type = "password" placeholder="Password" name = "Password"></td>
+        <td><input type = "password" placeholder="Password" name = "TherPassword"></td>
         </tr>   
         <tr>
             <td></td>
@@ -30,6 +59,7 @@
         </table>
         </form>
     </div>
+</div>
 
 </div>
 
