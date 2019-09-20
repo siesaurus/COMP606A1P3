e7d2dd3b1e21cb1553455cd7fd4d5260062635e7
Callum <callumgott@gmail.com>
2019-09-20 13:24:04 +1200

    editing the homepage to look more professional, adding some backstory and pictures

--- Home.php
+++ Home.php
@@ -14,10 +14,38 @@
             <h2>
                 Why Choose Us?
             </h2>   
+            <div class ="para-1">
             <p>
-                
+                We offer the best muscular physiotherapy services within the Waikato Region.
+                If you're suffering from an injury, we can help rehabilitate you and get you back
+                up to speed.
             </p>
+            <p>
+                Whether its from a sports injury, a fall, or just from repetitive strain,
+                we will strive to help you feel your best.
+            </p>
+            </div>
 
+            <!-- Royalty free physio pics from https://www.dreamstime.com/photos-images/physiotherapy.html -->
+            <div class="pic-container">
+                <div class="pics">
+                    <img src="Assets/pic1.jpg" alt="physio-pic"  height="125" width="250">                                              
+                    <img src="Assets/pic3.jpg" alt="physio-pic"  height="125" width="300">                                       
+                    <img src="Assets/pic2.jpg" alt="physio-pic"  height="125" width="250">                                    
+                 </div>
+
+             </div>    
+            <h2>
+                What we offer...
+            </h2>
+
+            <div class="para-1">
+                <p>
+                Short Massage
+                Medium Massage
+                Long Massage
+                </p>
+            </div>
         </div>
         
     </body>
