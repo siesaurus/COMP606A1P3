f2e5b52cea711ae9df4fa9b535ba1f6aa073f1fb
Callum <callumgott@gmail.com>
2019-09-23 20:35:44 +1200

    deleted the js link

--- welcomeTherapist.php
+++ welcomeTherapist.php
@@ -22,7 +22,7 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-<script type='text/javascript' src ="bookings.js"></script>
+
 <link rel="stylesheet" href="css/calendar.css">
     <meta charset="UTF-8">
     <title>Welcome, <?php echo $_SESSION['TherEmail'] ?></title>
